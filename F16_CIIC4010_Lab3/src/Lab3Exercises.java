219bf76c63640d06b590a7dfeb587af8b4de9293
4015c8 <4015c8@guanabana.ece.uprm.edu>
2016-08-31 12:14:12 -0400

    Changes on the name of the frame, location, and size. Also removing the commented line methos setDefaultCloseOperation.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("William Lugo Lamberty");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(800, 400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
